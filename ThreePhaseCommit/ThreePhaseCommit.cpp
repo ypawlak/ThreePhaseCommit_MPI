8f9b4abbdc83776165ad9290e675ae6741b61aeb
jpawlak <jan.pawlak@student.put.poznan.pl>
2018-02-22 16:54:57 +0100

    Add project files.

---
+++ ThreePhaseCommit/ThreePhaseCommit.cpp
@@ -0,0 +1,36 @@
+// ThreePhaseCommit.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "mpi.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include <ctime>
+#include <iostream>
+
+
+
+int main(int argc, char* argv[])
+{
+	MPI_Init(&argc, &argv);
+
+	int rank;
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	if (rank == 0)
+	{
+		char helloStr[] = "Hello World";
+		MPI_Send(helloStr, _countof(helloStr), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+	}
+	else if (rank == 1)
+	{
+		std::time_t result = std::time(nullptr);
+
+		char helloStr[12];
+		MPI_Recv(helloStr, _countof(helloStr), MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		printf("Rank 1 received string %s from Rank 0\n", helloStr);
+	}
+	MPI_Finalize();
+    return 0;
+}
+
